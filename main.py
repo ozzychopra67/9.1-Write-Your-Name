ddf99fceae02172de583ff1effd2747a43109692
Your Name <azichopratx@gmail.com>
2026-04-03 15:48:30 -0500

    finished

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from turtle import *
 paper = Screen()
 paper.bgcolor("Black")
 paper.title("Write your name")
-paper.setup(width = 1000, height = 500)
+paper.setup(width = 1200, height = 500)
 
 #forward,right,back,left
 def F():
     turt = Turtle()
+    turt.pensize(5)
     turt.color("white")
     turt.shape("turtle")
     turt.seth(90)
@@ -23,9 +24,10 @@ def F():
     turt.lt(90)
     turt.fd(70)
     turt.ht()
-
+F()
 def R():
     turt = Turtle()
+    turt.pensize(5)
     turt.color("Blue")
     turt.shape("turtle")
     turt.seth(90)
@@ -36,10 +38,78 @@ def R():
     for i in range(180):
         turt.fd(1)
         turt.seth(-i)
-    turt.seth(315)
+    turt.seth(300)
     turt.fd(100)
-    
-R()
+    turt.ht()
 
+R()
+    
+
+
+def U():
+    turt = Turtle()
+    turt.pensize(5)
+    turt.color("Yellow")
+    turt.shape("turtle")
+    turt.seth(270)
+    turt.pu()
+    turt.goto(-25,100)
+    turt.pd()
+    turt.forward(150)      
+    turt.circle(50, 180)   
+    turt.forward(150)
+    turt.ht()
+U()
+
+
+
+
+
+def I():
+    turt = Turtle()
+    turt.pensize(5)
+    turt.color("red")
+    turt.shape("turtle")
+    turt.seth(90)
+    turt.pu()
+    turt.goto(200,-100)
+    turt.pd()
+    turt.goto(200,100)
+    turt.lt(90)
+    turt.goto(150,100)
+    turt.goto(250,100)
+    turt.pu()
+    turt.goto(250,-100)
+    turt.pd()
+    turt.goto(150,-100)
+    turt.ht()
+
+I()
+
+def T():
+    turt = Turtle()
+    turt.pensize(5)
+    turt.color("green")
+    turt.shape("turtle")
+    turt.seth(270)
+    turt.pu()
+    turt.goto(350,100)
+    turt.pd()
+    turt.rt(90)
+    turt.fd(50)
+    turt.lt(180)
+    turt.goto(400,100)
+    turt.pu()
+    turt.goto(350,-100)
+    turt.lt(90)
+    turt.pd()
+    turt.fd(200)
+    turt.ht()
+
+
+
+
+T()
+    
 
 paper.exitonclick()
